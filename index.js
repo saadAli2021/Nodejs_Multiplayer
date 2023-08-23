2b0e9707ecd1a9dc5127898664b53121467031f9
saadali2021 <saadfiver4@gmail.com>
2023-08-23 14:32:00 +0500

    added rooms functionality

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ wss.on("connection", (ws) => {
   ws.on("message", (message) => {
     try {
       const playerData = JSON.parse(message);
-      handle_ReceivedMessages(playerData.action, playerData.payload);
+      handle_ReceivedMessages(ws, playerData.action, playerData.payload);
     } catch (error) {
       console.error("Error parsing JSON:", error);
     }
@@ -23,6 +23,12 @@ wss.on("connection", (ws) => {
 wss.on("listening", () => {
   handle_ServerIsReady();
 });
+// ********************** VARIABLES ***************************************
+const playerInfoMap = new Map(); // Map to store connected clients
+const rooms = new Map();
+
+let playerIdCounter = 0;
+let roomIdCounter = 0;
 // ********************** EVENTS HANDLERS *********************************
 
 // Handler when server is ready and starts listening for incoming connections
@@ -37,10 +43,18 @@ function handle_ClientDisconnect(code, reason) {
 function handle_NewConnection(ws) {}
 
 // A function to Handle received data from the client
-function handle_ReceivedMessages(action, payload) {
+function handle_ReceivedMessages(ws, action, payload) {
   switch (action) {
-    case "playerData": {
-      handlePlayerData(action, payload);
+    case "newConnection": {
+      onPlayerJoin(ws, action, payload);
+      break;
+    }
+    case "joinOrCreateRoom": {
+      joinOrCreateRoom(ws, payload.playerId);
+      break;
+    }
+    case "joinRoomById": {
+      joinRoomById(ws, payload.roomID, payload.playerId);
       break;
     }
     default: {
@@ -50,24 +64,77 @@ function handle_ReceivedMessages(action, payload) {
   }
 }
 
-/* ============================= helper functoisn ======================================== */
+function onPlayerJoin(ws, action, payload) {
+  idCounter++;
+  playerInfoMap.set(idCounter, { playerName: "Alice" });
+}
 
-function sendPlayerData(ws) {
-  const playerData = {
-    action: "playerData",
-    payload: {
-      msgType: "playerData",
-      playerName: "Alice",
-      id: 123,
-      colors: ["Red", "Green", "Blue", "Yellow", "Purple"],
-    },
-  };
-  const jsonPlayerData = JSON.stringify(playerData);
-  ws.send(jsonPlayerData);
+/* ============================= SUB EVENTS ======================================== */
+
+function joinRoomById(ws, roomID, playerID) {
+  const roomToJoin = rooms.get(roomID);
+  const currentPlayersInRoom = roomToJoin.currentPlayers.count;
+  if (currentPlayersInRoom < 4) {
+    roomToJoin.currentPlayers.push(playerID);
+  } else {
+    console.log("Error : Room is already Full");
+  }
 }
-function handlePlayerData(action, payload) {
-  console.log("Player Message Type:", payload.msgType);
-  console.log("Player Name:", payload.playerName);
-  console.log("ID:", payload.id);
-  console.log("Colors:", payload.colors);
+
+function joinOrCreateRoom(ws, playerId) {
+  let targetRoom = null;
+  // find any empty space in the room
+  for (const [roomID, room] of rooms) {
+    const currentPlayersCount = getCurrentPlayersCount(roomID);
+    if (currentPlayersCount < 4) {
+      targetRoom = room;
+      break;
+    }
+  }
+  // Creating A new Room if no space is available in any room
+  if (targetRoom === null) {
+    roomIdCounter++;
+    const newRoom = {
+      roomName: "Room" + roomIdCounter,
+      currentPlayers: [playerId],
+    };
+    targetRoom = newRoom;
+    rooms.set(roomIdCounter, newRoom);
+  }
+  // joint the target room if space is available
+  targetRoom.currentPlayers.push(playerId);
+
+  return newRoom;
 }
+
+function getCurrentPlayersCount(roomId) {
+  const room = rooms.get(roomId);
+
+  if (room) {
+    const currentPlayersCount = room.currentPlayers.length;
+    return currentPlayersCount;
+  } else {
+    return 0; // Room not found
+  }
+}
+
+/* ============================= Temporary Test functions ======================================== */
+// function sendPlayerData(ws) {
+//   const playerData = {
+//     action: "playerData",
+//     payload: {
+//       msgType: "playerData",
+//       playerName: "Alice",
+//       id: 123,
+//       colors: ["Red", "Green", "Blue", "Yellow", "Purple"],
+//     },
+//   };
+//   const jsonPlayerData = JSON.stringify(playerData);
+//   ws.send(jsonPlayerData);
+// }
+// function handlePlayerData(action, payload) {
+//   console.log("Player Message Type:", payload.msgType);
+//   console.log("Player Name:", payload.playerName);
+//   console.log("ID:", payload.id);
+//   console.log("Colors:", payload.colors);
+// }
